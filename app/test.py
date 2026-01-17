7cfa1ec53a06e85217be33cb1553fd44a791c72d
LLWICK <177215471+LLWICK@users.noreply.github.com>
2026-01-17 15:17:25 +0530

    yolov26 testing

---
+++ app/test.py
@@ -0,0 +1,18 @@
+from ultralytics import YOLO
+from multiprocessing import freeze_support
+
+def main():
+
+    # Load a COCO-pretrained YOLO26n model
+    model = YOLO("yolo26n.pt")
+
+    # Train the model on the COCO8 example dataset for 100 epochs
+    #results = model.train(data="coco8.yaml", epochs=100, imgsz=640)
+
+    # Run inference with the YOLO26n model on the 'bus.jpg' image
+    results = model("C:/Users/CHAMA COMPUTERS/Desktop/Data_Science/AI_ML/projects/YOLO26_demo/YOLO26_Demo/app/images/image1.jpeg")
+    print
+
+if __name__ == "__main__":
+    freeze_support()   # Safe on Windows
+    main()
